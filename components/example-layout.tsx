580e6972e7f1ffaacbfff72598dca4e943816da8
Fan Hu <hufan@shouqianba.com>
2024-04-12 14:16:38 +0800

    feat: 测试

--- components/example-layout.tsx
+++ components/example-layout.tsx
@@ -1,4 +1,4 @@
-export default function ExampleLayout({ children }) {
+export default function ExampleLayout({ children: any }) {
   return (
     <div className="text-blue-600">{children}</div>
   )
--- components/layout.tsx
+++ components/layout.tsx
@@ -1,6 +1,6 @@
 import SideNav from './sidenav'
  
-export default function Layout({ children }) {
+export default function Layout({ children: any }) {
   return (
     <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
       <div className="w-full flex-none md:w-64">
