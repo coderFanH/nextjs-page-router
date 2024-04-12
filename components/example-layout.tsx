d4a9f2e4e632b3033bbcd816c5892e1c2961601c
Fan Hu <hufan@shouqianba.com>
2024-04-12 14:20:00 +0800

    feat: 类型修改

--- components/example-layout.tsx
+++ components/example-layout.tsx
@@ -1,4 +1,4 @@
-export default function ExampleLayout({ children: any }) {
+export default function ExampleLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="text-blue-600">{children}</div>
   )
