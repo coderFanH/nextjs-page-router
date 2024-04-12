8526f6cadf9c16a35775236ce79591e1aa8e9954
Fan Hu <hufan@shouqianba.com>
2024-04-12 14:25:01 +0800

    feat: 类型校验

--- pages/example/content/index.tsx
+++ pages/example/content/index.tsx
@@ -1,6 +1,6 @@
 import { RenderingInfo } from "@/components/rendering-info"
 
-export const getStaticProps = async (context) => {
+export const getStaticProps = async () => {
   const res = await fetch('https://jsonplaceholder.typicode.com/posts/1')
   const data = await res.json()
   return { props: { data } }
@@ -12,7 +12,7 @@ export const getStaticProps = async (context) => {
 //   return { props: { data } }
 // }
 
-export default function Content({data}) {
+export default function Content({data}: {data: {body: string}}) {
 
   return (
     <div>
--- pages/example/scontent/index.tsx
+++ pages/example/scontent/index.tsx
@@ -5,13 +5,13 @@
 // }
 import { RenderingInfo } from "@/components/rendering-info"
 
-export const getServerSideProps = async (context) => {
+export const getServerSideProps = async () => {
   const res = await fetch('https://jsonplaceholder.typicode.com/posts/1')
   const data = await res.json()
   return { props: { data } }
 }
 
-export default function Content({data}) {
+export default function Content({data}: {data: {body: string}}) {
 
   return (
     <div>
